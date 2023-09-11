0772f276a90696f100486feb2fb03f8f7f731369
ichouare <issam.chouaref1998@gmail.com>
2023-09-11 18:46:35 +0100

    here

---
+++ CPP07/ex00/whatever.hpp
@@ -0,0 +1,14 @@
+#ifndef WHATER_H 
+#define WHATER_H
+#include <iostream>
+
+template <typename T>
+void swap(T  & a, T  & b);
+template <typename T>
+T const& min(T const & a, T const & b);
+template <typename T>
+T const & max(T const & a, T const & b);
+
+
+
+#endif
